64e7e1c3e23cdc1507cd21917ca1765c6d6d3e57
zhaogeng <827552776@qq.com>
2019-11-18 10:33:22 +0800

    first commit

---
+++ src/views/Sys/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input></input>
+    <i> 你好！世界！</i>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "helloWord"
+    }
+</script>
+
+<style scoped>
+
+</style>
