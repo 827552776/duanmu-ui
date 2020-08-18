b89642752d6de06f33ed65d024f13ddc7022a5a1
a1349433592 <lk927lk>
2020-08-18 10:02:09 +0800

    0818

--- src/views/Core/CoTable.vue
+++ src/views/Core/CoTable.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <el-tooltip content="导出" placement="top">
+      <el-button icon="fa fa-file-excel-o" @click="leading"> 导出excel</el-button>
+    </el-tooltip>
     <!--表格栏-->
     <el-table :data="data.content" :highlight-current-row="highlightCurrentRow" @selection-change="selectionChange"
           @current-change="handleCurrentChange" v-loading="loading" :element-loading-text="$t('action.loading')" :border="border" :stripe="stripe"
           :show-overflow-tooltip="showOverflowTooltip" :max-height="maxHeight" :height="height" :size="size" :align="align" style="width:100%;" :row-dblclick="handleEdit">
+    <el-table-column type="selection" width="40" v-if="showBatchDelete & showOperation"></el-table-column>
       <el-table-column type="index" width="40" ></el-table-column>
       <el-table-column v-for="column in columns" header-align="center" align="center" show-overflow-tooltip	="true"
         :prop="column.prop" :label="column.label" :width="column.width" :min-width="column.minWidth"
@@ -29,6 +33,7 @@
 
 <script>
 import KtButton from "@/views/Core/KtButton"
+import Export2Excel from "../../excel/Export2Excel";
 export default {
   name: 'KtTable',
   components:{
@@ -82,6 +87,8 @@ export default {
   },
   data() {
     return {
+      multipleSelection: [],
+      excelData: [],
       // 分页信息
 			pageRequest: {
 				pageNum: 1,
@@ -92,6 +99,43 @@ export default {
     }
   },
   methods: {
+    //选择导出数据
+    selectionChange(val) {
+      this.multipleSelection = val
+    },
+    leading() {
+      if (this.multipleSelection.length == 0) {
+        this.$alert('请选择要导出的信息', '提示', {
+          confirmButtonText: '确定',
+          callback: action => {}
+        });
+      } else {
+        this.exportExcel()
+      }
+    },
+    exportExcel() {
+      const header = ["批号", "客户", "模具名称", "派工数量", "入库单号", "入库数量", "入库时间", "出库数量",
+        "出库时间", "单位", "派工号", "属性", "开票时间", "发票号", "备注"] // 导出的表头名
+      const filterVal = ["lotNo", "cust", "mouldNm", "quantity", "wareNo", "wareNum", "wareDate", "outNum",
+        "outDate", "company", "dispatchNo", "shuxing", "kaipiaoTime", "billNo", "remarks"]
+      for (let i = 0; i < this.multipleSelection.length; i++) {
+        this.excelData.push(this.multipleSelection[i]);
+      }
+      const list = this.excelData
+      const data = this.formatJson(filterVal, list)
+
+      const filename = '已完成模具' + (new Date()).toLocaleDateString();
+      Export2Excel.export_json_to_excel({
+        header,
+        data,
+        filename
+      })
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        return v[j]
+      }))
+    },
     // 分页查询
     findPage: function () {
         this.loading = true
@@ -101,10 +145,10 @@ export default {
       this.$emit('findPage', {pageRequest:this.pageRequest, callback:callback})
     },
     // 选择切换
-    selectionChange: function (selections) {
-      this.selections = selections
-      this.$emit('selectionChange', {selections:selections})
-    },
+    // selectionChange: function (selections) {
+    //   this.selections = selections
+    //   this.$emit('selectionChange', {selections:selections})
+    // },
     // 选择切换
     handleCurrentChange: function (val) {
       this.$emit('handleCurrentChange', {val:val})
